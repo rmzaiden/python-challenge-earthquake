37dec3760cb585b9c85ecf9c179f01da50a2eb6e
Rodrigo Zaiden <rzaiden@ciandt.com>
2024-04-21 23:30:06 -0300

    improvements

--- src/main.py
+++ src/main.py
@@ -1,11 +1,9 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
 
-from controllers.city_controller import city_router as city_router
+from controllers.city_controller import city_router
 from controllers.country_controller import country_router
 from controllers.earthquake_controller import earthquake_router
-from controllers.earthquake_controller import \
-    earthquake_router as earthquake_router
 from controllers.state_controller import state_router
 
 load_dotenv()
