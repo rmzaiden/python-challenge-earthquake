a16302274d1929ed69d80001b941f3406c2d70a4
Rodrigo Zaiden <rzaiden@ciandt.com>
2024-04-23 22:31:33 -0300

    improvement earthquakes tests scenarios

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,7 +9,6 @@ client = TestClient(app)
 
 def test_get_notfoud_earthquakes_routers():
     """
-    Test case for the get_notfoud_earthquakes_routers function.
     Scenario: The user tries to get the earthquakes information providing a city name, a start date and an end date and returns no results.
     """
     response = client.get(
@@ -26,22 +25,50 @@ def test_get_notfoud_earthquakes_routers():
 
 def test_get_foud_earthquakes_routers():
     """
-    Test case for the get_foud_earthquakes_routers function.
     Scenario: The user tries to get the earthquakes information providing a city name, a start date and an end date and returns the closest earthquake to the city.
     """
     response = client.get(
         "/v1/earthquakes/",
         params={
             "city_name": "Los Angeles, CA",
-            "start_date": "2021-01-01",
-            "end_date": "2021-02-01",
+            "start_date": "2024-01-01",
+            "end_date": "2024-05-01",
         },
     )
     assert response.status_code == 200, response.text
     assert response.json() == {
-        "message": "The closest earthquake to Los Angeles, CA was an M 5.8 - Unknown location on January 15"
+        "message": "The closest earthquake to Los Angeles, CA was an M 5 - México on January 24"
     }
 
+def test_set_invalid_start_date_earthquakes_routers():
+    """
+    Scenario: The user tries to get the earthquakes information providing a city name, a invalid start date and a valid end date and returns validation error message.
+    """
+    response = client.get(
+        "/v1/earthquakes/",
+        params={
+            "city_name": "Los Angeles, CA",
+            "start_date": "2024-011-01",
+            "end_date": "2024-05-01",
+        },
+    )
+    assert response.status_code == 400, response.text
+    assert response.json() == {"detail":"1 validation error for EarthquakeModel\nstart_date\n  Input should be a valid date or datetime, invalid date separator, expected `-` [type=date_from_datetime_parsing, input_value='2024-011-01', input_type=str]\n    For further information visit https://errors.pydantic.dev/2.7/v/date_from_datetime_parsing"}
+
+def test_set_invalid_end_date_earthquakes_routers():
+    """
+    Scenario: The user tries to get the earthquakes information providing a city name, a valid start date and an invalid end date and returns validation error message.
+    """
+    response = client.get(
+        "/v1/earthquakes/",
+        params={
+            "city_name": "Los Angeles, CA",
+            "start_date": "2024-01-01",
+            "end_date": "2024-011-01",
+        },
+    )
+    assert response.status_code == 400, response.text
+    assert response.json() == {"detail":"1 validation error for EarthquakeModel\nend_date\n  Input should be a valid date or datetime, invalid date separator, expected `-` [type=date_from_datetime_parsing, input_value='2024-011-01', input_type=str]\n    For further information visit https://errors.pydantic.dev/2.7/v/date_from_datetime_parsing"}
 
 @patch("controllers.city_controller.create_city")
 def test_add_city_success(mock_create_city):
@@ -115,6 +142,7 @@ def test_add_city_with_invalid_state_province(mock_create_city):
         ]
     }
 
+
 @patch("controllers.city_controller.create_city")
 def test_add_city_with_invalid_state_country(mock_create_city):
 
@@ -139,7 +167,7 @@ def test_add_city_with_invalid_state_country(mock_create_city):
                 "input": "a",
             }
         ]
-    }    
+    }
 
 
 @patch("controllers.city_controller.create_city")
