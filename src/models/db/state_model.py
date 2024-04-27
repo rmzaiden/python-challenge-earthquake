39fb4e71262bec8367cb4c318043021f69070bfd
Rodrigo Zaiden <rzaiden@ciandt.com>
2024-04-27 12:52:31 -0300

    add column to state model

--- src/models/db/state_model.py
+++ src/models/db/state_model.py
@@ -8,5 +8,6 @@ class State(Base):
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     name = Column(String(100), nullable=False)
+    state_abbreviation = Column(String(2), nullable=True)
     country_id = Column(Integer, ForeignKey("countries.id"), nullable=False)
     __table_args__ = (UniqueConstraint("name", "country_id", name="_state_country_uc"),)
--- src/models/schemas/state_schema.py
+++ src/models/schemas/state_schema.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 
 class StateCreate(BaseModel):
     name: str
+    state_abbreviation: str
     country_id: int
 
 
